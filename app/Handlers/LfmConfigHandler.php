a31c8b2095424d3b3e985e50e90601589aea89d5
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-22 14:13:57 +0100

    test

--- app/Handlers/LfmConfigHandler.php
+++ app/Handlers/LfmConfigHandler.php
@@ -6,14 +6,6 @@ class ConfigHandler
 {
     public function userField()
     {
-        //$user = Auth::user();
-
-        // // Comprueba si el usuario es administrador
-        // if (!$user->role == 1) {
-        //     return $user->id . ' ' . str_replace(' ', '_', $user->name) . str_replace(' ', '_', $user->surname);
-        // }else{
-        //     return '';
-        // }
         return auth()->id();
     }
 }
