daa067585f3a983d1321b67b0076e2c01b56ca99
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-03-22 13:59:37 +0100

    test

--- app/Handlers/LfmConfigHandler.php
+++ app/Handlers/LfmConfigHandler.php
@@ -10,12 +10,18 @@ class ConfigHandler
     {
         $user = Auth::user();
 
-        // Comprueba si el usuario es administrador
-        if (!$user->role == 1) {
-            return $user->id ;
-        }else{
+        // // Comprueba si el usuario es administrador
+        // if (!$user->role == 1) {
+        //     return $user->id . ' ' . str_replace(' ', '_', $user->name) . str_replace(' ', '_', $user->surname);
+        // }else{
+        //     return '';
+        // }
+        if ($user->role == 1) {
+            // Administrador, devuelve una cadena vacía para permitir acceso a todo
             return '';
+        } else {
+            // Usuario no administrador, devuelve su ID para restringir acceso a su carpeta
+            return 'user_' . $user->id;
         }
-
     }
 }
